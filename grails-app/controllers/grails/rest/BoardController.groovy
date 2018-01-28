b64b151873d7d01cb54b9504ebb7239d8d2c971f
didin jamaludin <didinj@gmail.com>
2018-01-28 20:52:25 +0700

    first commit

---
+++ grails-app/controllers/grails/rest/BoardController.groovy
@@ -0,0 +1,11 @@
+package grails.rest
+
+import grails.rest.*
+import grails.converters.*
+
+class BoardController extends RestfulController {
+    static responseFormats = ['json', 'xml']
+    BoardController() {
+        super(Board)
+    }
+}
